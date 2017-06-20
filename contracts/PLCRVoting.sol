35acae2b5e4bc35e1a816096f764f2d8d85b2043
Yorke Rhodes IV <yorke.rhodesiv@consensys.net>
2017-06-20 15:25:09 -0400

    Define data structures

--- contracts/PLCRVoting.sol
+++ contracts/PLCRVoting.sol
@@ -9,25 +9,17 @@ contract Voting {
 
 	mapping(uint => Poll) pollMap;
 
-	// ARRAY WAY OF STORING USER VOTES:
-	// struct Votes {
-	// 	uint[] pollId;
-	// 	uint[] numTokens;
-	// 	bytes32[] hashCommit;
-	// }
-	// mapping(address => Votes) voteMap;
-	// Pro: We don't need to store user to pollId list to iterate
-	// Con: To store latest commit, we would have to know index of 
-	//      specific polls
-	// Result: cost incurred on user
-
-	// MAPPING WAY OF STORING USER VOTES:
-	struct Vote {
+	struct VoteNode {
+		bytes32 commitHash;
 		uint numTokens;
-		bytes32 hashCommit;
-	}	
-	mapping(address => mapping(uint => Vote)) voteMap;
-	// Pro: We can overwrite latest commit in constant time
-	// Con: We need to store list of PollIDs to iterate for max
-	// Result: cost incurred on contract owner
+		uint previousID;
+		uint nextID;
+	}
+
+	mapping(bytes32 => VoteNode) voteMap;
+
+	uint commitDuration;
+	uint revealDuration;
+	uint voteQuota;
+	address[] trusted;
 }
