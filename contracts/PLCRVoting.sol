12b55527078f40244d3f7b606ab777462ba0958c
Cemozerr <cemozer2018@u.northwestern.edu>
2017-06-21 10:44:18 -0400

    Insert helper functions from Aspyn

--- contracts/PLCRVoting.sol
+++ contracts/PLCRVoting.sol
@@ -14,10 +14,18 @@ contract Voting {
 	/// sha3(userAddress, pollID, "nextID") => byte32 nextID
 	/// sha3(userAddress, pollID, "numTokens") => byte32 numTokens
 	/// sha3(userAddress, pollID, "commitHash") => byte32 commitHash
-	mapping(bytes32 => bytes32) dll;  
+	mapping(bytes32 => bytes32) voteMap;  
 
 	uint commitDuration;	/// length of commit period
 	uint revealDuration;	/// length of reveal period
 	uint voteQuota;		/// type of majority necessary for winning poll
 	address[] trusted;	/// list of trusted addresses
+
+	function getAttribute(bytes32 pollID, string attrName) returns (bytes32) {
+          return voteMap[sha3(msg.sender, pollID, attrName)];
+	}
+
+	function setAttribute(bytes32 pollID, string attrName, bytes32 attrVal) {
+          voteMap[sha3(msg.sender, pollID, attrName)] = attrVal;
+	}
 }
