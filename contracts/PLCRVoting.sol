aae6564187b295e9b3d022068e1f506373d47681
Stuckinaboot <aspyn@seas.upenn.edu>
2017-06-21 10:12:19 -0400

    Add helper functions for moving VoteNode struct to being represented using the mapping (with no struct)

--- contracts/PLCRVoting.sol
+++ contracts/PLCRVoting.sol
@@ -28,6 +28,46 @@ contract Voting {
 	bytes32 constant ZERO_NODE_COMMIT_HASH = 0xabc;
 	bytes32 constant NODE_INVALID_COMMIT_HASH = 0x1337;
 
+	function commitVote(uint pollID, 
+		bytes32 hashOfVoteAndSalt, uint numTokens, 
+		uint prevPollID) commitPeriodActive(pollID) 
+		returns (bool) {
+
+		VoteNode nodeToInsert;
+
+		if (pollID == prevPollID) {
+			// Prepare an existing node for validation
+			nodeToInsert = voteMap[pollID];
+			nodeToInsert.commitHash = hashOfVoteAndSalt;
+			nodeToInsert.numTokens = numTokens;
+		} else {
+			// Prepare a new node for validation
+			nodeToInsert = VoteNode({
+				commitHash: hashOfVoteAndSalt,
+				numTokens: numTokens,
+				previousID: prevPollID
+			});
+		}
+
+		VoteNode node = validateNode(nodeToInsert);
+
+		// Node is valid
+		if (node.commitHash != NODE_INVALID_COMMIT_HASH) {
+			// Update a previous commit
+			if (node.pollID == prevPollID) {
+			/*
+				TODO: Update the <node at poll ID> to equal <node>
+			*/
+			} else {
+			/*
+			TODO: Insert node into double linked-list
+			*/
+		}
+
+		// Invalid prevPollID
+		return false;
+	}
+
 	function validateNode(VoteNode potentialNode) 
 		returns (VoteNode) {
 			VoteNode prevNode = voteMap[sha3(msg.sender, prevPollID)];
@@ -57,4 +97,28 @@ contract Voting {
 			return potentialNode;
 		}
 
+	function getPreviousID(bytes32 pollID) returns (bytes32) {
+		return getAttribute(pollID, "prevID");
+	}
+
+	function getNextID(bytes32 pollID) returns (bytes32) {
+		return getAttribute(pollID, "nextID");
+	}
+
+	function getNumTokens(bytes32 pollID) returns (bytes32) {
+		return getAttribute(pollID, "numTokens");
+	}
+
+	function getCommitHash(bytes32 pollID) returns (bytes32) {
+		return getAttribute(pollID, "commitHash");
+	}
+
+	function getAttribute(bytes32 pollID, string attrName) returns (bytes32) {
+		return voteMap[sha3(msg.sender, pollID, attrName)];
+	}
+
+	function setAttribute(bytes32 pollID, 
+		string attrName, bytes32 attrVal) {
+		voteMap[sha3(msg.sender, pollID, attrName)] = attrVal;
+	}
 }
