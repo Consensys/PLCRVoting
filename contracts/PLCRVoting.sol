cf1158aa9c0ee644f4e149d7cc692f67bbf56511
Cemozerr <cemozer2018@u.northwestern.edu>
2017-06-21 11:21:33 -0400

    Implement insertToDll function

--- contracts/PLCRVoting.sol
+++ contracts/PLCRVoting.sol
@@ -20,12 +20,31 @@ contract Voting {
 	uint revealDuration;	/// length of reveal period
 	uint voteQuota;		/// type of majority necessary for winning poll
 	address[] trusted;	/// list of trusted addresses
+        
+        function insertToDll(uint pollID, uint prevID, uint numTokens, bytes32 commitHash) returns (bool) {
 
-	function getAttribute(bytes32 pollID, string attrName) returns (bytes32) {
+          uint nextID = uint(getAttribute(bytes32 prevID, "nextID"));
+
+          // make nextNode.prev point to newNode
+          setAttribute(nextID, "prevID", bytes32(pollID))
+
+          // make prevNode.next point to newNode
+          setAttribute(prevID, "nextID", bytes32(pollID));
+
+          // make newNode point to next and prev 
+          setAttribute(pollID, "prevID", bytes32(prevID)); 
+          setAttribute(pollID, "prevID", bytes32(nextID)); 
+
+          // set properties of newNode
+          setAttribute(pollID, "numTokens", bytes32(numTokens));
+          setAttribute(pollID, "commitHash", bytes32(commitHash));
+        }
+
+	function getAttribute(uint pollID, string attrName) returns (bytes32) {
           return voteMap[sha3(msg.sender, pollID, attrName)];
 	}
 
-	function setAttribute(bytes32 pollID, string attrName, bytes32 attrVal) {
+	function setAttribute(uint pollID, string attrName, bytes32 attrVal) {
           voteMap[sha3(msg.sender, pollID, attrName)] = attrVal;
 	}
 }
