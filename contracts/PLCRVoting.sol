348756026b1ecb1c4d68404328a1893cd25059b3
Cemozerr <cemozer2018@u.northwestern.edu>
2017-06-22 17:55:53 -0400

    Change VoteMap type, fix helper functions
    The VoteMap mapping is now bytes32 => uint instead of bytes32 => bytes32, and
    thus helper functions, except getCommitHash, returns uints. The reason this type
    change was made is that when uint variables implicitly got cast to bytes32, which
    happened during mapping, they would end up on the left side of the hexadecimal
    number instead of the right side, causing the number to be extraordinarily large.
    
    Helper functions were fixed according to the change above, and to protect the
    integrity of commitHashs, a seperate helper function to get it from mapping was
    implemented.
    
    Lastly, to increase the ease of testing, a helper function that produces the
    exact sha3 hash in Javascript that Solidity produces when hashing an address,
    a uint and a string was implemented and named createIndexHash. This is needed
    since a hash of an address, a uint and a string is used as index for the VoteMap
    mapping.

--- contracts/PLCRVoting.sol
+++ contracts/PLCRVoting.sol
@@ -1,52 +1,62 @@
 pragma solidity ^0.4.4;
 
 contract PLCRVoting {
-	struct Poll {
-		uint commitEndDate; /// expiration date of commit period for poll
-		uint revealEndDate; /// expiration date of reveal period for poll
-		uint voteQuota;	    /// snapshot of canonical voteQuota
-		uint votesFor;	    /// tally of votes supporting proposal
-		uint votesAgainst;  /// tally of votes countering proposal
-	}
-	/// maps pollID to Poll struct
-	mapping(uint => Poll) pollMap; 
+  struct Poll {
+    uint commitEndDate;     /// expiration date of commit period for 
+    uint revealEndDate;     /// expiration date of reveal period for 
+    uint voteQuota;         /// snapshot of canonical voteQ
+    uint votesFor;          /// tally of votes supporting prop
+    uint votesAgainst;      /// tally of votes countering proposal
+  }
+  // maps pollID to Poll struct
+  mapping(uint => Poll) public pollMap; 
 
-        /// represent a double linked list through mapping
-	/// sha3(userAddress, pollID, "prevID") => byte32 prevID
-	/// sha3(userAddress, pollID, "nextID") => byte32 nextID
-	/// sha3(userAddress, pollID, "numTokens") => byte32 numTokens
-	/// sha3(userAddress, pollID, "commitHash") => byte32 commitHash
-	mapping(bytes32 => bytes32) voteMap;  
+  // represent a double linked list through mapping
+  // sha3(userAddress, pollID, "prevID") => byte32 prevID
+  // sha3(userAddress, pollID, "nextID") => byte32 nextID
+  // sha3(userAddress, pollID, "numTokens") => byte32 numTokens
+  // sha3(userAddress, pollID, "commitHash") => byte32 commitHash
+  mapping(bytes32 => uint) public voteMap;  
 
-	uint commitDuration;	/// length of commit period
-	uint revealDuration;	/// length of reveal period
-	uint voteQuota;		/// type of majority necessary for winning poll
-	address[] trusted;	/// list of trusted addresses
-        
-        function insertToDll(uint pollID, uint prevID, uint numTokens, bytes32 commitHash) returns (bool) {
+  uint commitDuration;      /// length of commit period
+  uint revealDuration;      /// length of reveal period
+  uint voteQuota;           /// type of majority necessary for winning poll
+  address[] trusted;        /// list of trusted addresses
+   
+ /* 
+  function insertToDll(uint pollID, uint prevID, uint numTokens, bytes32 commitHash){
+    uint nextID = uint(getAttribute(bytes32 prevID, "nextID"));
 
-          uint nextID = uint(getAttribute(bytes32 prevID, "nextID"));
+    // make nextNode.prev point to newNode
+    setAttribute(nextID, "prevID", bytes32(pollID));
 
-          // make nextNode.prev point to newNode
-          setAttribute(nextID, "prevID", bytes32(pollID))
+    // make prevNode.next point to newNode
+    setAttribute(prevID, "nextID", bytes32(pollID));
 
-          // make prevNode.next point to newNode
-          setAttribute(prevID, "nextID", bytes32(pollID));
+    // make newNode point to next and prev 
+    setAttribute(pollID, "prevID", bytes32(prevID)); 
+    setAttribute(pollID, "prevID", bytes32(nextID)); 
 
-          // make newNode point to next and prev 
-          setAttribute(pollID, "prevID", bytes32(prevID)); 
-          setAttribute(pollID, "prevID", bytes32(nextID)); 
+    // set properties of newNode
+    setAttribute(pollID, "numTokens", bytes32(numTokens));
+    setAttribute(pollID, "commitHash", bytes32(commitHash));
+  }
+ */
 
-          // set properties of newNode
-          setAttribute(pollID, "numTokens", bytes32(numTokens));
-          setAttribute(pollID, "commitHash", bytes32(commitHash));
-        }
+ /*
+  * Helper Functions
+  */
+ 
+  // get any attribute that is not commitHash
+  function getAttribute(uint pollID, string attrName) returns (uint) {
+    return voteMap[sha3(msg.sender, pollID, attrName)];
+  }
 
-	function getAttribute(uint pollID, string attrName) returns (bytes32) {
-          return voteMap[sha3(msg.sender, pollID, attrName)];
-	}
+  function getCommitHash(uint pollID) returns (bytes32) {
+    return bytes32(voteMap[sha3(msg.sender, pollID, 'commitHash')]);
+  }
 
-	function setAttribute(uint pollID, string attrName, bytes32 attrVal) {
-          voteMap[sha3(msg.sender, pollID, attrName)] = attrVal;
-	}
+  function setAttribute(uint pollID, string attrName, uint attrVal) {
+    voteMap[sha3(msg.sender, pollID, attrName)] = attrVal;
+  }
 }
--- test/sample.js
+++ test/sample.js
@@ -1,14 +1,77 @@
-var PLCRVoting = artifacts.require("./PLCRVoting.sol");
+const abi = require('ethereumjs-abi');
+const BN = require('bn.js');
+const PLCRVoting = artifacts.require("./PLCRVoting.sol");
 
 contract('Voting', function(accounts) {
-  it("should return 10", function() {
+  /*
+   * Utility Functions
+   */
+
+  // returns the solidity-sha3 output for VoteMap indexing
+  function createIndexHash(account, pollID, atr) {
+    let hash = "0x" + abi.soliditySHA3(
+          [ "address", "uint", "string" ],
+          [ new BN(account.slice(2), 16), pollID, atr ]
+    ).toString('hex'); 
+    return hash;
+  }
+
+  /*
+   * Tests
+   */
+
+
+  it("getAttribute returns 0 when voteMap empty", function() {
     return PLCRVoting.deployed()
     .then(function(instance) {
-      return instance.foobar.call();
+      return instance.getAttribute.call('10', 'prevID');
     })
     .then(function(result) {
-      console.log(result)
-      assert.equal(result, 10, "Testing failed.");
+      assert.equal(Number(result), '0', "Testing failed.");
     });
   });
+
+  it("setAttribute sets property correctly", function() {
+    let voting;
+    return PLCRVoting.deployed()
+    .then(function(instance) {
+      voting = instance;
+      voting.setAttribute('10', 'prevID', '25', {from:accounts[0]});
+      return voting.voteMap.call(
+        createIndexHash(accounts[0], '10', 'prevID')
+      );
+    })
+    .then(function(result) {
+      assert.equal(result, '25', "Testing failed.");
+    });
+  });
+
+  it("getAttribute gets previously set property correctly", function() {
+    let voting;
+    return PLCRVoting.deployed()
+    .then(function(instance) {
+      voting = instance;
+      voting.setAttribute('10', 'prevID', '15');
+      return voting.getAttribute.call('10', 'prevID');
+    })
+    .then(function(result) {
+      assert.equal(result,'15', "Testing failed");
+    });
+  });
+
+  it("getCommitHash returns correct commitHash", function() {
+    let voting;
+    let hash;
+    return PLCRVoting.deployed()
+    .then(function(instance) {
+      voting = instance;
+      hash = '0x1c817d209103a160ddcfed5d97912c7e207da04a8a9960c64875cecec0bfb0de';
+      voting.setAttribute('10', 'commitHash', hash);
+      return voting.getCommitHash.call('10');
+    })
+    .then(function(result) {
+      assert.equal(result, hash, "Testing failed");
+    });
+  });
+
 });
