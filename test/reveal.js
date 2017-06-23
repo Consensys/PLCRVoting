874fe33398ed53ae5a9c8101d19c4cb4a2bdbe22
Stuckinaboot <aspyn@seas.upenn.edu>
2017-06-23 10:59:10 -0400

    Add test headers for reveal

---
+++ test/reveal.js
@@ -0,0 +1,34 @@
+var PLCRVoting = artifacts.require("./PLCRVoting.sol");
+
+contract('Voting', function(accounts) {
+  it("single reveal for single commit to single poll", function() {
+    return PLCRVoting.deployed()
+    .then(function(instance) {
+    })
+  });
+  it("single reveal for no commits to single poll", function() {
+    return PLCRVoting.deployed()
+    .then(function(instance) {
+    })
+  });
+  it("single reveal different vote selection for single commit to single poll", function() {
+    return PLCRVoting.deployed()
+    .then(function(instance) {
+    })
+  });
+  it("three reveals for three commits (different senders) to single poll", function() {
+    return PLCRVoting.deployed()
+    .then(function(instance) {
+    })
+  });
+  it("single reveal after reveal expiration date", function() {
+    return PLCRVoting.deployed()
+    .then(function(instance) {
+    })
+  });
+  it("double reveal attempt (by single sender) for single poll", function() {
+    return PLCRVoting.deployed()
+    .then(function(instance) {
+    })
+  });
+});
