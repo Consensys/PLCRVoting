2cf7fb271748651f33893334b06335241ac7a990
Yorke Rhodes IV <yorke.rhodesiv@consensys.net>
2017-06-23 14:29:58 -0400

    Modify migration script to match truffle recommended pattern

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,13 +8,11 @@ const fs = require(`fs`);
 module.exports = (deployer, network, accounts) => {
 	let tokenConf = JSON.parse(fs.readFileSync('./conf/testToken.json'));
 
-	return deployer.deploy(
-		TokenContract,
+	deployer.deploy(TokenContract,
 		tokenConf.initialAmount,
 		tokenConf.tokenName,
 		tokenConf.decimalUnits,
 		tokenConf.tokenSymbol
 	)
-	.then(() => TokenContract.deployed())
-	.then((token) => deployer.deploy(VotingContract, token));
+	.then(() => deployer.deploy(VotingContract, TokenContract.address));
 };
