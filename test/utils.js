4a9618e1dd93aff43d1100a40c284a0ad2d01cd6
Mike Goldin <mike.goldin@consensys.net>
2017-09-07 18:06:43 -0400

    Use object literal syntax

--- test/utils.js
+++ test/utils.js
@@ -13,70 +13,58 @@ const ethQuery = new EthQuery(new HttpProvider('http://localhost:8545'));
 
 const utils = {
   // returns the solidity-sha3 output for VoteMap indexing
-  createIndexHash(account, pollID, atr) {
+  createIndexHash: (account, pollID, atr) => {
     const hash = `0x${abi.soliditySHA3(['address', 'uint', 'string'],
       [account, pollID, atr]).toString('hex')}`;
     return hash;
   },
 
   // returns the solidity-sha3 output for vote hashing
-  createVoteHash(vote, salt) {
+  createVoteHash: (vote, salt) => {
     const hash = `0x${abi.soliditySHA3(['uint', 'uint'],
       [vote, salt]).toString('hex')}`;
     return hash;
   },
 
   // returns block timestamp
-  getBlockTimestamp() {
-    return ethQuery.blockNumber()
-      .then(num => ethQuery.getBlockByNumber(num, true))
-      .then(block => block.timestamp.toString(10));
-  },
+  getBlockTimestamp: () => ethQuery.blockNumber()
+    .then(num => ethQuery.getBlockByNumber(num, true))
+    .then(block => block.timestamp.toString(10)),
 
   // returns Token instance
-  getERC20Token() {
-    return this.getVoteContract()
-      .then(vote => vote.token.call())
-      .then(tokenAddr => HumanStandardToken.at(tokenAddr));
-  },
+  getERC20Token: () => utils.getPLCRInstance()
+    .then(vote => vote.token.call())
+    .then(tokenAddr => HumanStandardToken.at(tokenAddr)),
 
   // returns poll instance
-  getPoll(pollID) {
-    return this.getVoteContract()
-      .then(instance => instance.pollMap.call(pollID));
-  },
+  getPoll: pollID => utils.getPLCRInstance()
+    .then(instance => instance.pollMap.call(pollID)),
 
   // returns deployed vote contract
-  getPLCRInstance() {
-    return PLCRVoting.deployed();
-  },
+  getPLCRInstance: () => PLCRVoting.deployed(),
 
   // increases time
-  increaseTime(seconds) {
-    return new Promise((resolve, reject) => ethRPC.sendAsync({
-      method: 'evm_increaseTime',
-      params: [seconds],
+  increaseTime: seconds => new Promise((resolve, reject) => ethRPC.sendAsync({
+    method: 'evm_increaseTime',
+    params: [seconds],
+  }, (err) => {
+    if (err) reject(err);
+    resolve();
+  }))
+    .then(() => new Promise((resolve, reject) => ethRPC.sendAsync({
+      method: 'evm_mine',
+      params: [],
     }, (err) => {
       if (err) reject(err);
       resolve();
-    }))
-      .then(() => new Promise((resolve, reject) => ethRPC.sendAsync({
-        method: 'evm_mine',
-        params: [],
-      }, (err) => {
-        if (err) reject(err);
-        resolve();
-      })));
-  },
+    }))),
 
   // launches poll and returns pollID as string
-  launchPoll(proposal, commitDuration, revealDuration) {
-    return this.getVoteContract()
-      .then(vote => vote.startPoll(0, commitDuration, revealDuration))
-      .then(result => result.logs[0].args.pollID.toString());
-  },
+  launchPoll: (voteQuorum, commitDuration, revealDuration) => utils.getPLCRInstance()
+    .then(vote => vote.startPoll(voteQuorum, commitDuration, revealDuration))
+    .then(result => result.logs[0].args.pollID.toString()),
 
-  as(actor, fn, ...args) {
+  as: (actor, fn, ...args) => {
     function detectSendObject(potentialSendObj) {
       function hasOwnProperty(obj, prop) {
         const proto = obj.constructor.prototype;
