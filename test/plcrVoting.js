220773d364b9008b86ec515d2c68f81c607075f1
Mike Goldin <mike.goldin@consensys.net>
2017-09-11 11:21:09 -0400

    Add positive assertions

--- test/plcrVoting.js
+++ test/plcrVoting.js
@@ -352,12 +352,12 @@ contract('PLCRVoting', (accounts) => {
   describe('Function: getInsertPointForNumTokens', () => {
     const [alice] = accounts;
 
-    // TODO: Improve this test with positive assertions
     it('should return the correct insert point for a new node in a DLL', async () => {
       // Create { A: 1, B: 5, C: 10 }
       // Then insert { A: 1, D: 3, B: 5, C: 10 }
       // And then { A: 1, D: 3, B: 5, E: 7, C: 10 }
       const plcr = await utils.getPLCRInstance();
+      const errMsg = 'Did not get proper insertion point';
 
       await utils.as(alice, plcr.requestVotingRights, 50);
 
@@ -365,6 +365,7 @@ contract('PLCRVoting', (accounts) => {
       let secretHash = utils.createVoteHash(1, 420);
       let numTokens = 1;
       let insertPoint = await plcr.getInsertPointForNumTokens.call(alice, numTokens);
+      assert(insertPoint.toString(10), '0', errMsg); // after root
       await utils.as(alice, plcr.commitVote, pollID, secretHash, numTokens, insertPoint);
 
       // { A: 1 }
@@ -373,6 +374,7 @@ contract('PLCRVoting', (accounts) => {
       secretHash = utils.createVoteHash(1, 420);
       numTokens = 5;
       insertPoint = await plcr.getInsertPointForNumTokens.call(alice, numTokens);
+      assert(insertPoint.toString(10), '1', errMsg); // after A
       await utils.as(alice, plcr.commitVote, pollID, secretHash, numTokens, insertPoint);
 
       // { A: 1, B: 5 }
@@ -381,6 +383,7 @@ contract('PLCRVoting', (accounts) => {
       secretHash = utils.createVoteHash(1, 420);
       numTokens = 10;
       insertPoint = await plcr.getInsertPointForNumTokens.call(alice, numTokens);
+      assert(insertPoint.toString(10), '2', errMsg); // after B
       await utils.as(alice, plcr.commitVote, pollID, secretHash, numTokens, insertPoint);
 
       // { A: 1, B: 5, C: 10 }
@@ -389,6 +392,7 @@ contract('PLCRVoting', (accounts) => {
       secretHash = utils.createVoteHash(1, 420);
       numTokens = 3;
       insertPoint = await plcr.getInsertPointForNumTokens.call(alice, numTokens);
+      assert(insertPoint.toString(10), '1', errMsg); // after A 
       await utils.as(alice, plcr.commitVote, pollID, secretHash, numTokens, insertPoint);
 
       // { A: 1, D: 3, B: 5, C: 10 }
@@ -397,6 +401,7 @@ contract('PLCRVoting', (accounts) => {
       secretHash = utils.createVoteHash(1, 420);
       numTokens = 7;
       insertPoint = await plcr.getInsertPointForNumTokens.call(alice, numTokens);
+      assert(insertPoint.toString(10), '2', errMsg); // after B 
       await utils.as(alice, plcr.commitVote, pollID, secretHash, numTokens, insertPoint);
 
       // { A: 1, D: 3, B: 5, E: 7, C: 10 }
