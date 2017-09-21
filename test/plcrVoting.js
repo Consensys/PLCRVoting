85ce34c845cdfce89d1978164d1d8c87aa04021c
Isaac Kang <isaacjkang@gmail.com>
2017-09-21 16:13:23 -0400

    Add test

--- test/plcrVoting.js
+++ test/plcrVoting.js
@@ -527,8 +527,19 @@ contract('PLCRVoting', () => {
   });
 });
 
-contract('PLCRVoting', () => {
+contract('PLCRVoting', (accounts) => {
+  const [alice] = accounts;
+
   describe('Function: hasBeenRevealed', () => {
+    it('should fail if the poll does not exist', async () => {
+      const plcr = await utils.getPLCRInstance();
+      try {
+        await plcr.hasBeenRevealed(alice, 420);
+        assert(false, 'should have failed for non-existant poll 420');
+      } catch (err) {
+        assert(utils.isEVMException(err), err.toString());
+      }
+    });
     it('should return true if the user has already revealed for this poll');
     it('should return false if the user has not revealed for this poll');
   });
