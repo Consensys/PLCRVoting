bb11f15286fd46276e6bc167c8b4b41af5fa24dd
Isaac Kang <isaacjkang@gmail.com>
2017-09-21 16:04:39 -0400

    Add underscores to AttributeStore args

--- contracts/AttributeStore.sol
+++ contracts/AttributeStore.sol
@@ -5,13 +5,13 @@ library AttributeStore {
         mapping(bytes32 => uint) store;
     }
 
-    function getAttribute(Data storage self, bytes32 UUID, string attrName) returns (uint) {
-        bytes32 key = sha3(UUID, attrName);
+    function getAttribute(Data storage self, bytes32 _UUID, string _attrName) returns (uint) {
+        bytes32 key = sha3(_UUID, _attrName);
         return self.store[key];
     }
 
-    function attachAttribute(Data storage self, bytes32 UUID, string attrName, uint attrVal) {
-        bytes32 key = sha3(UUID, attrName);
-        self.store[key] = attrVal;
+    function attachAttribute(Data storage self, bytes32 _UUID, string _attrName, uint _attrVal) {
+        bytes32 key = sha3(_UUID, _attrName);
+        self.store[key] = _attrVal;
     }
 }
