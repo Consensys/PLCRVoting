cc56046cfcd29945b32b37ec7d696d5025291256
Isaac Kang <isaacjkang@gmail.com>
2017-09-21 16:07:42 -0400

    Change test to pollExists

--- test/plcrVoting.js
+++ test/plcrVoting.js
@@ -639,7 +639,7 @@ contract('PLCRVoting', () => {
 });
 
 contract('PLCRVoting', () => {
-  describe('Function: doesExist', () => {
+  describe('Function: pollExists', () => {
     it('should return true if a poll with pollID exists');
     it('should return false if a poll with pollID does not exist');
   });
