5dcc5e089446cf963bd678f1806eea6687f0ecd6
Mike Goldin <mike.goldin@consensys.net>
2017-10-31 18:19:35 -0400

    Use Truffle 4

--- truffle.js
+++ truffle.js
@@ -14,11 +14,6 @@ if (fs.existsSync('secrets.json')) {
 
 module.exports = {
   networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // Match any network id
-    },
     rinkeby: {
       provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
       network_id: '*',
