eb3eec0a32951df508401275a7531a5a17433213
Mike Goldin <mike.goldin@consensys.net>
2017-12-07 12:14:59 -0500

    add INITIAL_POLL_NONCE test

--- test/INITIAL_POLL_NONCE.js
+++ test/INITIAL_POLL_NONCE.js
@@ -1,9 +1,16 @@
 /* eslint-env mocha */
-/* global contract */
+/* global contract assert */
+
+const utils = require('./utils.js');
 
 contract('PLCRVoting', () => {
   describe('Function: INITIAL_POLL_NONCE', () => {
-    it('should be zero');
+    it('should be zero', async () => {
+      const plcr = await utils.getPLCRInstance();
+
+      assert.strictEqual((await plcr.INITIAL_POLL_NONCE.call()).toString(10), '0',
+        'The INITIAL_POLL_NONCE was not initialized to zero');
+    });
   });
 });
 
