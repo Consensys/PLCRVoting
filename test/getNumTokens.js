2d20b68c3258d0272d2621b6a38ad3f4bb1bb274
Isaac Kang <isaac.kang@consensys.net>
2017-12-27 13:45:12 -0800

    Add test/getNumTokens

--- test/getNumTokens.js
+++ test/getNumTokens.js
@@ -1,9 +1,35 @@
 /* eslint-env mocha */
-/* global contract */
+/* global contract assert */
 
-contract('PLCRVoting', () => {
+const utils = require('./utils.js');
+
+contract('PLCRVoting', (accounts) => {
   describe('Function: getNumTokens', () => {
-    it('should return the number of tokens committed by the voter for some pollID');
+    const [alice] = accounts;
+    it('should return the number of tokens committed by the voter for some pollID', async () => {
+      const plcr = await utils.getPLCRInstance();
+      const token = await utils.getERC20Token();
+      const options = utils.defaultOptions();
+      options.actor = alice;
+      options.numTokens = '40';
+
+      const startingBalance = await token.balanceOf.call(alice);
+      const pollID = await utils.startPollAndCommitVote(options);
+
+      const numTokens = await utils.as(alice, plcr.getNumTokens, alice, pollID);
+      assert.strictEqual(
+        numTokens.toString(10),
+        '40',
+        'should have 40 numTokens',
+      );
+
+      const endBalance = await token.balanceOf.call(alice);
+      assert.strictEqual(
+        endBalance.toString(10),
+        startingBalance.sub(options.votingRights).toString(10),
+        'should have 50 less tokens because of voting rights',
+      );
+    });
   });
 });
 
