6257eef3e5cfc72764f6e06c388c5a0320fc70a2
Cemozerr <cemozer2018@u.northwestern.edu>
2018-02-21 19:39:14 -0600

    Test failure for getCommitHash

--- test/getCommitHash.js
+++ test/getCommitHash.js
@@ -5,7 +5,7 @@ const utils = require('./utils.js');
 
 contract('PLCRVoting', (accounts) => {
   describe('Function: getCommitHash', () => {
-    const [alice] = accounts;
+    const [alice, bob] = accounts;
 
     it('should return the commit hash stored by the voter for some pollID',
       async () => {
@@ -21,7 +21,26 @@ contract('PLCRVoting', (accounts) => {
         assert.strictEqual(secretHash, retreivedHash, errMsg);
     });
 
-    it('should fail if the user has not stored any commit hash for some pollID');
+    it('should fail if the user has not stored any commit hash for some pollID',
+      async () => {
+        const errMsg = 'Bob was able to retreive a commit hash although he had not committed any vote';
+        const plcr = await utils.getPLCRInstance();
+        const options = utils.defaultOptions();
+
+        // Start poll and commit vote as Alice
+        options.actor = alice;
+        const pollID = await utils.startPollAndCommitVote(options);
+
+        try {
+          // try to get commit hash for Bob
+          options.actor = bob;
+          const retreivedHash = await plcr.getCommitHash.call(options.actor, pollID);
+          assert(false, errMsg);
+        } catch (err) {
+          assert(utils.isEVMException(err), err.toString());
+          return;
+        }
+    });
   });
 });
 
