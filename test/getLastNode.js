6403505bc28bb94a5a9b0320c00e0226565c97a2
Cemozerr <cemozer2018@u.northwestern.edu>
2018-02-21 21:01:56 -0600

    Test getLastNode

--- test/getLastNode.js
+++ test/getLastNode.js
@@ -1,9 +1,45 @@
 /* eslint-env mocha */
 /* global contract */
 
-contract('PLCRVoting', () => {
+const utils = require('./utils.js');
+
+contract('PLCRVoting', (accounts) => {
   describe('Function: getLastNode', () => {
-    it('should return the poll for which the user has the greatest number of tokens committed');
+    const [alice] = accounts;
+
+    it('should return the poll for which the user has the greatest number of tokens committed',
+      async () => {
+        const errMsg = 'Alice could not retreive the correct last node';
+        const plcr = await utils.getPLCRInstance();
+        const options = utils.defaultOptions();
+        options.actor = alice;
+
+        let retreivedPollID;
+        let highestVotePollID;
+
+        // Alice commits only to one poll and retreives that node
+        options.numTokens = '10';
+        highestVotePollID = await utils.startPollAndCommitVote(options);
+        retreivedPollID = await plcr.getLastNode.call(options.actor);
+        assert.strictEqual(highestVotePollID.toString(10),
+                           retreivedPollID.toString(10),
+                           errMsg);
+
+        // Alice commits to another poll but with less tokens than his first time
+        options.numTokens = '5';
+        await utils.startPollAndCommitVote(options);
+        retreivedPollID = await plcr.getLastNode.call(options.actor);
+        assert.strictEqual(highestVotePollID.toString(10),
+                           retreivedPollID.toString(10),
+                           errMsg);
+
+        // Alice commits to another poll but with more tokens than his first time
+        options.numTokens = '20';
+        highestVotePollID = await utils.startPollAndCommitVote(options);
+        retreivedPollID = await plcr.getLastNode.call(options.actor);
+        assert.strictEqual(highestVotePollID.toString(10),
+                           retreivedPollID.toString(10),
+                           errMsg);
+    });
   });
 });
-
