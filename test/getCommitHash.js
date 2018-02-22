1689a6c7e5c071ec3e302a158cb3259183106768
Cemozerr <cemozer2018@u.northwestern.edu>
2018-02-21 19:33:34 -0600

    Test correct behaviour for getCommitHash

--- test/getCommitHash.js
+++ test/getCommitHash.js
@@ -1,9 +1,26 @@
 /* eslint-env mocha */
 /* global contract */
 
-contract('PLCRVoting', () => {
+const utils = require('./utils.js');
+
+contract('PLCRVoting', (accounts) => {
   describe('Function: getCommitHash', () => {
-    it('should return the commit hash stored by the voter for some pollID');
+    const [alice] = accounts;
+
+    it('should return the commit hash stored by the voter for some pollID',
+      async () => {
+        const errMsg = 'Alice could not retreive the correct commit hash';
+        const plcr = await utils.getPLCRInstance();
+        const options = utils.defaultOptions();
+        options.actor = alice;
+
+        const pollID = await utils.startPollAndCommitVote(options);
+        const secretHash = utils.createVoteHash(options.vote, options.salt);
+
+        const retreivedHash = await plcr.getCommitHash.call(options.actor, pollID);
+        assert.strictEqual(secretHash, retreivedHash, errMsg);
+    });
+
     it('should fail if the user has not stored any commit hash for some pollID');
   });
 });
