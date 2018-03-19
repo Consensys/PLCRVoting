26579ec7f645f868cdb4c7de4c764808882a5d3a
Kmoneal <kmacjoneal@gmail.com>
2018-03-19 13:55:07 -0700

    Address audit finding 3.3
    Overflows are not checked when computing the commitEndDate and
    revealEndDate in startPoll. This can result in polls that will accept
    votes, but never unlock tokens.
    
    * Import Zeppelin SafeMath through EPM.
    * Use SafeMath to calculate commitEndDate and revealEndDate in
      startPoll.
    * Add two tests to startPoll.js checking for overflows for the commit
      and reveal durations.

--- test/startPoll.js
+++ test/startPoll.js
@@ -1,5 +1,9 @@
 /* eslint-env mocha */
-/* global contract */
+/* global contract assert artifacts */
+
+const PLCRVoting = artifacts.require('./PLCRVoting.sol');
+const utils = require('./utils.js');
+const BN = require('bignumber.js');
 
 contract('PLCRVoting', () => {
   describe('Function: startPoll', () => {
@@ -8,5 +12,41 @@ contract('PLCRVoting', () => {
     it('should create a poll with a 50% vote quorum and 100 second commit/reveal durations');
     it('should create a poll with a 60% vote quorum, a 100 second commit duration and ' +
        'a 200 second reveal duration');
+
+    it('should revert if block timestamp plus provided _commitDuration is greater than 2^256-1', async () => {
+      const plcr = await PLCRVoting.deployed();
+
+      // getting the maximum of uint and storing in maxEVMuint
+      const maxEVMuint = new BN('2').pow('256').minus('1');
+      const blockTimestamp = await utils.getBlockTimestamp();
+      // setting commitDuration to block macEVMuint - blockTimestamp
+      const commitDuration = maxEVMuint.minus(blockTimestamp).plus('1');
+
+      try {
+        await plcr.startPoll('50', commitDuration, '0');
+      } catch (err) {
+        assert(err.toString().includes('invalid opcode'), err.toString());
+        return;
+      }
+      assert(false, 'Expected revert not recieved.');
+    });
+
+    it('should revert if (block timestamp + _commitDuration) plus provided _revealDuration is greater than 2^256-1', async () => {
+      const plcr = await PLCRVoting.deployed();
+
+      // getting the maximum of uint and storing in maxEVMuint
+      const maxEVMuint = new BN('2').pow('256').minus('1');
+      const blockTimestamp = await utils.getBlockTimestamp();
+      // setting revealDuration to block macEVMuint - blockTimestamp
+      const revealDuration = maxEVMuint.minus(blockTimestamp).plus('1');
+
+      try {
+        await plcr.startPoll('50', '0', revealDuration);
+      } catch (err) {
+        assert(err.toString().includes('invalid opcode'), err.toString());
+        return;
+      }
+      assert(false, 'Expected revert not recieved.');
+    });
   });
 });
