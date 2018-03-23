a5e930af85c565a0780f6e930347911a0ca30ca1
Isaac Kang <isaac.kang@consensys.net>
2018-03-23 10:30:23 -0400

    reduce hardcode in getLockedTokens

--- test/getLockedTokens.js
+++ test/getLockedTokens.js
@@ -2,11 +2,12 @@
 /* global contract assert */
 
 const utils = require('./utils.js');
+const BN = require('bignumber.js');
 
 contract('PLCRVoting', (accounts) => {
-  describe('Function: getLockedTokens', () => {
-    const [alice] = accounts;
+  const [alice] = accounts;
 
+  describe('Function: getLockedTokens', () => {
     it('should return the number of tokens the user has locked in polls', async () => {
       const plcr = await utils.getPLCRInstance();
       const token = await utils.getERC20Token();
@@ -15,18 +16,18 @@ contract('PLCRVoting', (accounts) => {
 
       const startingBalance = await token.balanceOf.call(alice);
       const pollID = await utils.startPollAndCommitVote(options);
-      await utils.increaseTime(101);
+      await utils.increaseTime(new BN(options.commitPeriod, 10).add(new BN('1', 10)).toNumber(10));
 
       const middleBalance = await token.balanceOf.call(alice);
       const locked = await utils.as(alice, plcr.getLockedTokens, alice);
-      assert.strictEqual(locked.toString(10), '20', 'Should have 20 tokens locked up');
+      assert.strictEqual(locked.toString(10), options.numTokens, 'Should have 20 tokens locked up');
 
       await utils.as(alice, plcr.revealVote, pollID, options.vote, options.salt);
       const notLocked = await utils.as(alice, plcr.getLockedTokens, alice);
       const finalBalance = await token.balanceOf.call(alice);
 
       assert.strictEqual(notLocked.toString(10), '0', 'Should have 0 tokens locked up');
-      assert.strictEqual(startingBalance.sub(middleBalance).toString(10), '50',
+      assert.strictEqual(startingBalance.sub(middleBalance).toString(10), options.votingRights,
         'Should have reduced by the default amount',
       );
       assert.strictEqual(middleBalance.toString(10), finalBalance.toString(10),
