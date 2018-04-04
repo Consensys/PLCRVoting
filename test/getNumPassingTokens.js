ad0cba0647118adf8f61fe288bf1c81e0b172c6b
Akua Nti <akua.nti@consensys.net>
2018-04-03 21:19:56 -0400

    Add test coverage to PLCRVoting
    Bring statement coverage to 100% and branch coverage to 96.55%.
    
    * Add tests
      * getNumPassingTokens
      * getTotalNumberOfTokensForWinningOption
      * isPassed
    * Add more helpers to utils.js

--- test/getNumPassingTokens.js
+++ test/getNumPassingTokens.js
@@ -7,6 +7,7 @@ const BN = require('bignumber.js');
 contract('PLCRVoting', (accounts) => {
   describe('Function: getNumPassingTokens', () => {
     const [alice] = accounts;
+
     describe('should correctly return the number of tokens that voted for the winning option', () => {
       it('voting for', async () => {
         const options = utils.defaultOptions();
@@ -15,11 +16,9 @@ contract('PLCRVoting', (accounts) => {
 
         const plcr = await utils.getPLCRInstance();
         const pollID = await utils.startPollAndCommitVote(options);
-
         await utils.increaseTime(new BN(options.commitPeriod, 10).add(new BN('1', 10)).toNumber(10));
 
         await utils.as(options.actor, plcr.revealVote, pollID, options.vote, options.salt);
-
         await utils.increaseTime(new BN(options.revealPeriod, 10).add(new BN('1', 10)).toNumber(10));
 
         const passingTokens = await plcr.getNumPassingTokens
@@ -36,11 +35,9 @@ contract('PLCRVoting', (accounts) => {
 
         const plcr = await utils.getPLCRInstance();
         const pollID = await utils.startPollAndCommitVote(options);
-
         await utils.increaseTime(new BN(options.commitPeriod, 10).add(new BN('1', 10)).toNumber(10));
 
         await utils.as(options.actor, plcr.revealVote, pollID, options.vote, options.salt);
-
         await utils.increaseTime(new BN(options.revealPeriod, 10).add(new BN('1', 10)).toNumber(10));
 
         const passingTokens = await plcr.getNumPassingTokens
@@ -51,8 +48,80 @@ contract('PLCRVoting', (accounts) => {
       });
     });
 
-    it('should fail if the poll queried has not yet ended');
-    it('should fail if the voter queried has not yer revealed');
+    it('should revert if the poll queried has not yet ended', async () => {
+      const options = utils.defaultOptions();
+      options.actor = alice;
+      options.vote = '0';
+
+      // make a poll and commit
+      const plcr = await utils.getPLCRInstance();
+      const pollID = await utils.startPollAndCommitVote(options);
+
+      // call before reveal end date
+      try {
+        await plcr.getNumPassingTokens.call(options.actor, pollID, options.salt);
+      } catch (err) {
+        assert(utils.isEVMException(err), err.toString());
+        return;
+      }
+      assert(false, 'was able to call getNumPassingTokens on a poll that was not finished');
+    });
+
+    it('should revert if the voter has not yet revealed a vote for the given poll', async () => {
+      const options = utils.defaultOptions();
+      options.actor = alice;
+      options.vote = '0';
+
+      // make a poll and commit
+      const plcr = await utils.getPLCRInstance();
+      const pollID = await utils.startPollAndCommitVote(options);
+
+      // end the poll, but do not reveal
+      const increase = new BN(options.commitPeriod, 10)
+        .add(new BN(options.revealPeriod, 10))
+        .add('1');
+      await utils.increaseTime(increase.toNumber(10));
+
+      // make sure the poll has ended
+      const ended = plcr.pollEnded.call(pollID);
+      assert(ended, 'poll has not ended!');
+
+      // call
+      try {
+        await plcr.getNumPassingTokens.call(options.actor, pollID, options.salt);
+      } catch (err) {
+        assert(utils.isEVMException(err), err.toString());
+        return;
+      }
+      assert(false, 'was able to call getNumPassingTokens on a poll without revealing a vote');
+    });
+
+    it('should revert if the calculated commitHash does not match the original', async () => {
+      const options = utils.defaultOptions();
+      options.actor = alice;
+      options.vote = '0';
+
+      // commit and reveal a vote
+      const plcr = await utils.getPLCRInstance();
+      const pollID = await utils.startPollAndCommitVote(options);
+      await utils.increaseTime(new BN(options.commitPeriod, 10).add('1').toNumber(10));
+
+      await utils.as(options.actor, plcr.revealVote, pollID, options.vote, options.salt);
+      await utils.increaseTime(new BN(options.revealPeriod, 10).add(new BN('1', 10)).toNumber(10));
+
+      // call with some other salt
+      const salt = '1776';
+      assert.notStrictEqual(salt, options.salt);
+
+      try {
+        await plcr.getNumPassingTokens.call(options.actor, pollID, salt);
+      } catch (err) {
+        assert(utils.isEVMException(err), err.toString());
+        return;
+      }
+      assert(false, 'was able to call getNumPassingTokens with a bad salt');
+    });
+
     it('should return 0 if the queried tokens were committed to the minority bloc');
   });
 });
--- test/isPassed.js
+++ test/isPassed.js
@@ -1,11 +1,120 @@
 /* eslint-env mocha */
-/* global contract */
+/* global contract assert */
 
-contract('PLCRVoting', () => {
+const utils = require('./utils.js');
+const BN = require('bignumber.js');
+
+contract('PLCRVoting', (accounts) => {
   describe('Function: isPassed', () => {
-    it('should return true if the poll passed');
-    it('should return false if the poll ended in a tie');
-    it('should return false if the poll did not pass');
+    const [alice, bob] = accounts;
+
+    it('should return true if the poll passed', async () => {
+      const options = utils.defaultOptions();
+      options.actor = alice;
+      options.vote = '1';
+
+      // make a poll and commit a vote for
+      const plcr = await utils.getPLCRInstance();
+      const pollID = await utils.startPollAndCommitVote(options);
+      await utils.increaseTime(new BN(options.commitPeriod, 10).add(new BN('1', 10)).toNumber(10));
+
+      await utils.as(options.actor, plcr.revealVote, pollID, options.vote, options.salt);
+      await utils.increaseTime(new BN(options.revealPeriod, 10).add(new BN('1', 10)).toNumber(10));
+
+      const isPassed = await plcr.isPassed.call(pollID);
+      assert.strictEqual(isPassed, true, 'isPassed should have returned true for a passing poll');
+    });
+
+    it('should return false if the poll ended in a tie', async () => {
+      const plcr = await utils.getPLCRInstance();
+      const aliceOptions = utils.defaultOptions();
+      aliceOptions.actor = alice;
+      aliceOptions.vote = '0';
+
+      const bobOptions = utils.defaultOptions();
+      bobOptions.actor = bob;
+      bobOptions.vote = '1';
+
+      const options = utils.defaultOptions();
+
+      // start the poll as alice
+      const receipt = await utils.as(alice, plcr.startPoll, options.quorum,
+        options.commitPeriod, options.revealPeriod);
+      const pollID = utils.getPollIDFromReceipt(receipt);
+
+      // commit for each voter
+      await utils.commitAs(pollID, aliceOptions);
+      await utils.commitAs(pollID, bobOptions);
+      await utils.increaseTime(new BN(options.commitPeriod, 10).add(new BN('1', 10)).toNumber(10));
+
+      // reveal for each voter
+      await utils.as(aliceOptions.actor, plcr.revealVote, pollID, aliceOptions.vote,
+        aliceOptions.salt);
+      await utils.as(bobOptions.actor, plcr.revealVote, pollID, bobOptions.vote, bobOptions.salt);
+      await utils.increaseTime(new BN(options.revealPeriod, 10).add(new BN('1', 10)).toNumber(10));
+
+      // should be 1-1 tie
+      const isPassed = await plcr.isPassed.call(pollID);
+      assert.strictEqual(isPassed, false, 'isPassed should have returned false for a tie');
+    });
+
+    it('should return false if the nobody voted', async () => {
+      const plcr = await utils.getPLCRInstance();
+      const options = utils.defaultOptions();
+      options.actor = alice;
+
+      // start the poll
+      const receipt = await utils.as(options.actor, plcr.startPoll, options.quorum,
+        options.commitPeriod, options.revealPeriod);
+      const pollID = utils.getPollIDFromReceipt(receipt);
+
+      // go to the end of the time period
+      const increase = new BN(options.commitPeriod, 10)
+        .add(new BN(options.revealPeriod, 10))
+        .add('1');
+      await utils.increaseTime(increase.toNumber(10));
+
+      const isPassed = await plcr.isPassed.call(pollID);
+      assert.strictEqual(isPassed, false, 'isPassed should have returned false for a tie');
+    });
+
+
+    it('should return false if the poll did not pass', async () => {
+      const options = utils.defaultOptions();
+      options.actor = alice;
+      options.vote = '0';
+
+      // make a poll and commit a vote against
+      const plcr = await utils.getPLCRInstance();
+      const pollID = await utils.startPollAndCommitVote(options);
+      await utils.increaseTime(new BN(options.commitPeriod, 10).add(new BN('1', 10)).toNumber(10));
+
+      await utils.as(options.actor, plcr.revealVote, pollID, options.vote, options.salt);
+      await utils.increaseTime(new BN(options.revealPeriod, 10).add(new BN('1', 10)).toNumber(10));
+
+      const isPassed = await plcr.isPassed.call(pollID);
+      assert.strictEqual(isPassed, false, 'isPassed should have returned false for a passing poll');
+    });
+
+    it('should revert if the poll has not ended', async () => {
+      // create a poll
+      const options = utils.defaultOptions();
+      options.actor = alice;
+      options.vote = '0';
+
+      // make a poll and commit
+      const plcr = await utils.getPLCRInstance();
+      const pollID = await utils.startPollAndCommitVote(options);
+
+      // call before reveal end date
+      try {
+        await plcr.isPassed.call(pollID);
+      } catch (err) {
+        assert(utils.isEVMException(err), err.toString());
+        return;
+      }
+      assert(false, 'was able to call isPassed on a poll that was not finished');
+    });
   });
 });
 
