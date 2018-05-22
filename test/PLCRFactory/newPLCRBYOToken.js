51d3ead6d9bc9f78b4ea47ba190c286f943aacb7
Mike Goldin <mike.goldin@consensys.net>
2018-05-21 20:22:31 -0400

    Change the test directory hierarchy

---
+++ test/PLCRFactory/newPLCRBYOToken.js
@@ -0,0 +1,36 @@
+/* eslint-env mocha */
+/* global contract assert artifacts */
+
+const PLCRFactory = artifacts.require('./PLCRFactory.sol');
+const PLCRVoting = artifacts.require('./PLCRVoting.sol');
+const EIP20 = artifacts.require('tokens/eip20/EIP20.sol');
+
+contract('PLCRFactory', () => {
+  describe('Function: newPLCRBYOToken', () => {
+    let plcrFactory;
+
+    before(async () => {
+      plcrFactory = await PLCRFactory.deployed();
+    });
+
+    it('should deploy and initialize a new PLCRVoting contract using the token at the passed-in ' +
+      'address', async () => {
+      const tokenParams = {
+        supply: '1000',
+        name: 'TEST',
+        decimals: '2',
+        symbol: 'TST',
+      };
+      const token = await EIP20.new(tokenParams.supply, tokenParams.name, tokenParams.decimals,
+        tokenParams.symbol);
+
+      const receipt = await plcrFactory.newPLCRBYOToken(token.address);
+      const plcr = PLCRVoting.at(receipt.logs[0].args.plcr);
+
+      const plcrToken = await plcr.token.call();
+
+      assert.strictEqual(plcrToken, token.address, 'the token attached to the PLCR contract does ' +
+        'not correspond to the one the user specified');
+    });
+  });
+});
