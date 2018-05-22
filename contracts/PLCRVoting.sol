5983d9cb63c5443fe4c321f2c8db808361452fc5
Isaac Kang <isaac.kang@consensys.net>
2018-05-22 16:22:20 -0400

    change function name, add tests
    * rescueTokensMultiPolls -> rescueTokensInMultiplePolls
    * add 4 tests

--- contracts/PLCRVoting.sol
+++ contracts/PLCRVoting.sol
@@ -109,7 +109,7 @@ contract PLCRVoting {
     @dev Unlocks tokens locked in unrevealed votes where polls have ended
     @param _pollIDs Array of integer identifiers associated with the target polls
     */
-    function rescueTokensMultiPolls(uint[] _pollIDs) public {
+    function rescueTokensInMultiplePolls(uint[] _pollIDs) public {
         // loop through arrays, rescuing tokens from all
         for (uint i = 0; i < _pollIDs.length; i++) {
             rescueTokens(_pollIDs[i]);
