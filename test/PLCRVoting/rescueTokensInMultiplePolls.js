b68a0b3b28102bbaac6715ff4e311eb204de6bba
Mike Goldin <mike.goldin@consensys.net>
2018-05-23 16:25:11 -0400

    Fix the failing rescueMultiTokens tests
    When the changes adding factory contracts were merged into develop, they
    changed the context available through Truffle to tests such that these
    tests broke. This commit fixes them.
    
    * Stop relying on truffle context in rescueMultiTokens tests
    * Use new APIs for relevant utility functions
    * Use fewer magic numbers, and add comments where magic numbers persist

--- test/PLCRVoting/rescueTokensInMultiplePolls.js
+++ test/PLCRVoting/rescueTokensInMultiplePolls.js
@@ -1,26 +1,45 @@
 /* eslint-env mocha */
-/* global contract assert */
+/* global contract assert artifacts */
+
+const PLCRFactory = artifacts.require('./PLCRFactory.sol');
+const PLCRVoting = artifacts.require('./PLCRVoting.sol');
+const EIP20 = artifacts.require('tokens/eip20/EIP20.sol');
 
 const utils = require('./utils.js');
 
 contract('PLCRVoting', (accounts) => {
   describe('Function: rescueTokensInMultiplePolls', () => {
     const [alice, bob] = accounts;
+    let plcr;
+    let token;
+
+    before(async () => {
+      const plcrFactory = await PLCRFactory.deployed();
+      const factoryReceipt = await plcrFactory.newPLCRWithToken('10000', 'TestToken', '0', 'TEST');
+      plcr = PLCRVoting.at(factoryReceipt.logs[0].args.plcr);
+      token = EIP20.at(factoryReceipt.logs[0].args.token);
+
+      await Promise.all(
+        accounts.map(async (user) => {
+          await token.transfer(user, 1000);
+          await token.approve(plcr.address, 1000, { from: user });
+        }),
+      );
+    });
 
     it('should enable the user to withdraw tokens they committed but did not reveal after ' +
     'an array of 1 poll has ended', async () => {
-      const plcr = await utils.getPLCRInstance();
-      const token = await utils.getERC20Token();
       const options = utils.defaultOptions();
       options.actor = alice;
 
       const startingBalance = await token.balanceOf.call(alice);
-      const pollID = await utils.startPollAndCommitVote(options);
+      const pollID = await utils.startPollAndCommitVote(options, plcr);
       const pollIDs = [pollID];
 
-      await utils.increaseTime(201);
+      await utils.increaseTime(parseInt(options.commitPeriod, 10) +
+        parseInt(options.revealPeriod, 10) + 1);
       await utils.as(alice, plcr.rescueTokensInMultiplePolls, pollIDs);
-      await utils.as(alice, plcr.withdrawVotingRights, 50);
+      await utils.as(alice, plcr.withdrawVotingRights, options.votingRights.toString());
 
       const finalBalance = await token.balanceOf.call(alice);
       assert.strictEqual(finalBalance.toString(10), startingBalance.toString(10),
@@ -29,20 +48,20 @@ contract('PLCRVoting', (accounts) => {
 
     it('should enable the user to withdraw tokens they committed but did not reveal after ' +
     'an array of 2 polls have ended', async () => {
-      const plcr = await utils.getPLCRInstance();
-      const token = await utils.getERC20Token();
       const options = utils.defaultOptions();
       options.actor = alice;
 
       const startingBalance = await token.balanceOf.call(alice);
-      const pollID1 = await utils.startPollAndCommitVote(options);
-      const pollID2 = await utils.startPollAndCommitVote(options);
+      const pollID1 = await utils.startPollAndCommitVote(options, plcr);
+      const pollID2 = await utils.startPollAndCommitVote(options, plcr);
 
       const pollIDs = [pollID1, pollID2];
 
-      await utils.increaseTime(201);
+      await utils.increaseTime(parseInt(options.commitPeriod, 10) +
+        parseInt(options.revealPeriod, 10) + 1);
       await utils.as(alice, plcr.rescueTokensInMultiplePolls, pollIDs);
-      await utils.as(alice, plcr.withdrawVotingRights, 100);
+      await utils.as(alice, plcr.withdrawVotingRights,
+        (parseInt(options.votingRights, 10) * 2).toString());
 
       const finalBalance = await token.balanceOf.call(alice);
       assert.strictEqual(finalBalance.toString(10), startingBalance.toString(10),
@@ -51,14 +70,16 @@ contract('PLCRVoting', (accounts) => {
 
     it('should not allow users to rescue tokens they committed before any polls in an array have ended',
       async () => {
-        const plcr = await utils.getPLCRInstance();
         const options = utils.defaultOptions();
         options.actor = bob;
 
-        const pollID1 = await utils.startPollAndCommitVote(options);
+        const pollID1 = await utils.startPollAndCommitVote(options, plcr);
+        // Increase time by half of the commit period
         await utils.increaseTime(50);
 
-        const pollID2 = await utils.startPollAndCommitVote(options);
+        const pollID2 = await utils.startPollAndCommitVote(options, plcr);
+        // Increase time such that poll 1's reveal period ends, but poll 2's reveal period remains
+        // in-flight. At this point 201 seconds have elapsed since poll 1 began, so it's over.
         await utils.increaseTime(151);
 
         // pollID1 has ended
@@ -79,9 +100,8 @@ contract('PLCRVoting', (accounts) => {
         assert(false, 'Bob was able to rescue unrevealed tokens before a poll ended');
       });
 
-    it('should throw an error when attempting to rescue tokens from an array of 1 non-existant poll',
+    it('should throw an error when attempting to rescue tokens from an array of 1 non-existant polls',
       async () => {
-        const plcr = await utils.getPLCRInstance();
         const options = utils.defaultOptions();
         options.actor = bob;
         const pollID = '667';
