090d9fe7a76085fa4e6150243cb27d3fcac19065
Isaac Kang <isaac.kang@consensys.net>
2018-12-18 16:18:55 -0800

    use async/await in migration

--- migrations/2_plcr_factory.js
+++ migrations/2_plcr_factory.js
@@ -4,14 +4,14 @@ const PLCRFactory = artifacts.require('./PLCRFactory.sol');
 const DLL = artifacts.require('dll/DLL.sol');
 const AttributeStore = artifacts.require('attrstore/AttributeStore.sol');
 
-module.exports = (deployer) => {
+module.exports = async (deployer) => {
   // deploy libraries
-  deployer.deploy(DLL);
-  deployer.deploy(AttributeStore);
+  await deployer.deploy(DLL);
+  await deployer.deploy(AttributeStore);
 
   // link libraries
-  deployer.link(DLL, PLCRFactory);
-  deployer.link(AttributeStore, PLCRFactory);
+  await deployer.link(DLL, PLCRFactory);
+  await deployer.link(AttributeStore, PLCRFactory);
 
-  deployer.deploy(PLCRFactory);
+  await deployer.deploy(PLCRFactory);
 };
